bd95a0dc037b2eb7c6ac7cffd3e619f2ef763071
ozanozcelik <ozanozclk0@gmail.com>
2024-10-31 00:54:50 +0300

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Ozan Ozcelik on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
