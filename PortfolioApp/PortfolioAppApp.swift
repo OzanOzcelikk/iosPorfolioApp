b200b6b6994ca99945d40f4298ad0e4cf7417943
ozanozcelik <ozanozclk0@gmail.com>
2024-11-02 23:04:18 +0300

    setup CoreData

--- PortfolioApp/PortfolioAppApp.swift
+++ PortfolioApp/PortfolioAppApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct PortfolioAppApp: App {
+    
+    @StateObject var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
