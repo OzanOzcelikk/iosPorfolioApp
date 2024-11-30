1d0022a1706e0c2b44b8dfa9f38d62d0baec07d3
Ozan Özçelik <ozanozcelik@192.168.1.107>
2024-11-30 14:43:21 +0300

    DataController bug fix

--- PortfolioApp/DataController.swift
+++ PortfolioApp/DataController.swift
@@ -10,181 +10,166 @@ import CoreData
 
 class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
-    
+
     @Published var selectedFilter: Filter? = Filter.all
     @Published var selectedIssue: Issue?
-    
-    @Published var filterText: String = ""
+
+    @Published var filterText = ""
     @Published var filterTokens = [Tag]()
-    
+
     private var saveTask: Task<Void, Error>?
-    
+
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
         dataController.createSampleData()
         return dataController
     }()
-    
-    var suggesteedFilterTokens: [Tag]{
-        
+
+    var suggestedFilterTokens: [Tag] {
         guard filterText.starts(with: "#") else {
-            return[]
+            return []
         }
-        
+
         let trimmedFilterText = String(filterText.dropFirst()).trimmingCharacters(in: .whitespaces)
         let request = Tag.fetchRequest()
-        
+
         if trimmedFilterText.isEmpty == false {
             request.predicate = NSPredicate(format: "name CONTAINS[c] %@", trimmedFilterText)
         }
-        
+
         return (try? container.viewContext.fetch(request).sorted()) ?? []
     }
 
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Main")
-        
+
         if inMemory {
-            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
         }
-        
+
         container.viewContext.automaticallyMergesChangesFromParent = true
         container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
-        
+
         container.persistentStoreDescriptions.first?.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
-        
         NotificationCenter.default.addObserver(forName: .NSPersistentStoreRemoteChange, object: container.persistentStoreCoordinator, queue: .main, using: remoteStoreChanged)
-        
+
         container.loadPersistentStores { storeDescription, error in
-            if let error = error {
+            if let error {
                 fatalError("Fatal error loading store: \(error.localizedDescription)")
             }
         }
     }
-    
-    func remoteStoreChanged(_ notification: Notification){
+
+    func remoteStoreChanged(_ notification: Notification) {
         objectWillChange.send()
     }
-    
-    
+
     func createSampleData() {
         let viewContext = container.viewContext
-        
-        for i in 1..<6 {
+
+        for i in 1...5 {
             let tag = Tag(context: viewContext)
             tag.id = UUID()
             tag.name = "Tag \(i)"
-            
-            for j in 1..<6 {
+
+            for j in 1...10 {
                 let issue = Issue(context: viewContext)
                 issue.title = "Issue \(i)-\(j)"
-                issue.content = "This is a test issue."
-                issue.creationDate = Date()
+                issue.content = "Description goes here"
+                issue.creationDate = .now
                 issue.completed = Bool.random()
                 issue.priority = Int16.random(in: 0...2)
                 tag.addToIssues(issue)
             }
         }
-        
+
         try? viewContext.save()
-        
     }
-    
-    func save(){
-        if container.viewContext.hasChanges{
+
+    func save() {
+        if container.viewContext.hasChanges {
             try? container.viewContext.save()
         }
     }
-    
-    func queueSave(){
+
+    func queueSave() {
         saveTask?.cancel()
-        
-        saveTask = Task{ @MainActor in
+
+        saveTask = Task { @MainActor in
             try await Task.sleep(for: .seconds(3))
             save()
         }
     }
-        
-    func delete(_ object: NSManagedObject){
+
+    func delete(_ object: NSManagedObject) {
         objectWillChange.send()
         container.viewContext.delete(object)
         save()
     }
 
-    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>){
+    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-        
         batchDeleteRequest.resultType = .resultTypeObjectIDs
-        
-        if let result = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult{
-            let changes = [NSDeletedObjectsKey: result.result as? [NSManagedObjectID] ?? []]
-            
+
+        if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
+            let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
             NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
         }
     }
-        
-        func deleteAll(){
-            let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
-            delete(request1)
-            
-            let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
-            delete(request2)
-            
-            save()
-        }
-    
-    
+
+    func deleteAll() {
+        let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
+        delete(request1)
+
+        let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
+        delete(request2)
+
+        save()
+    }
+
     func missingTags(from issue: Issue) -> [Tag] {
         let request = Tag.fetchRequest()
         let allTags = (try? container.viewContext.fetch(request)) ?? []
-        
+
         let allTagsSet = Set(allTags)
         let difference = allTagsSet.symmetricDifference(issue.issueTags)
-        
+
         return difference.sorted()
     }
-    
+
     func issuesForSelectedFilter() -> [Issue] {
-        
         let filter = selectedFilter ?? .all
-        
         var predicates = [NSPredicate]()
-        
+
         if let tag = filter.tag {
-            let tagPredicate = NSPredicate(format: "tags CONTAINS == %@", tag)
+            let tagPredicate = NSPredicate(format: "tags CONTAINS %@", tag)
             predicates.append(tagPredicate)
-        }else {
-            let datePredicate = NSPredicate(format: "modeficationDate > %@", filter.minModificationDate as NSDate)
+        } else {
+            let datePredicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
             predicates.append(datePredicate)
         }
-        
-        let trimmedFilterText = filter.name.trimmingCharacters(in: .whitespaces)
-        
+
+        let trimmedFilterText = filterText.trimmingCharacters(in: .whitespaces)
+
         if trimmedFilterText.isEmpty == false {
             let titlePredicate = NSPredicate(format: "title CONTAINS[c] %@", trimmedFilterText)
             let contentPredicate = NSPredicate(format: "content CONTAINS[c] %@", trimmedFilterText)
             let combinedPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [titlePredicate, contentPredicate])
-            
             predicates.append(combinedPredicate)
         }
-        
+
         if filterTokens.isEmpty == false {
             for filterToken in filterTokens {
-                let tokenPredicate = NSPredicate( format: "tags IN %@", filterToken)
+                let tokenPredicate = NSPredicate(format: "tags CONTAINS %@", filterToken)
                 predicates.append(tokenPredicate)
             }
-            
         }
-        
-        
+
         let request = Issue.fetchRequest()
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
-        let allIssues = (try? container.viewContext.fetch(request)) ?? []
-        
-        return allIssues.sorted()
-        
-    }
-        
 
+        let allIssues = (try? container.viewContext.fetch(request)) ?? []
+        return allIssues.sorted()
+    }
 }
 
